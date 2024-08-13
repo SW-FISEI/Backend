8f44082062a369c282d19fe541ffcee5892236ad
Christian Chico <93953404+ChristianCLop@users.noreply.github.com>
2024-08-13 10:12:02 -0500

    HotFix Usuarios DTO

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsString, MaxLength } from "class-validator";
+
 export class CreateUsuarioDto {
+
+    @IsString()
+    @MaxLength(30)
     nombre: string;
+
+    @IsEmail()
+    @MaxLength(50)
     email: string;
+
+    @IsString()
     contrasenia: string;
 }
